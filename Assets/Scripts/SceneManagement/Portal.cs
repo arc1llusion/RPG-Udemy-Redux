2e3229d73b472784f3c17cc8a1237671e277a355
Arc1llusion <arc1llusion@gmail.com>
2023-02-22 15:14:53 -0500

    Lesson 72

--- Assets/Scripts/SceneManagement/Portal.cs
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -1,6 +1,11 @@
+using RPG.Control;
+using RPG.Core;
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.SceneManagement;
 
 namespace RPG.SceneMnagement
@@ -10,13 +15,40 @@ namespace RPG.SceneMnagement
         [SerializeField]
         private int sceneToLoad = -1;
 
+        [SerializeField]
+        private Transform spawnPoint;
+
         private void OnTriggerEnter(Collider other)
         {
             if(other.gameObject.CompareTag("Player"))
             {
-                SceneManager.LoadScene(sceneToLoad);
-            }
-            
+                StartCoroutine(Transition());
+            }            
+        }
+
+        private IEnumerator Transition()
+        {
+            DontDestroyOnLoad(this);
+            yield return SceneManager.LoadSceneAsync(sceneToLoad);
+
+            Portal otherPortal = GetOtherPortal();
+            UpdatePlayer(otherPortal);
+
+            Destroy(this.gameObject);
+        }
+
+        private void UpdatePlayer(Portal otherPortal)
+        {
+            var player = GameObject.FindGameObjectWithTag("Player");
+            player.GetComponent<ActionScheduler>().CancelCurrentAction();
+            player.GetComponent<NavMeshAgent>().Warp(otherPortal.spawnPoint.position);
+            player.transform.rotation = otherPortal.spawnPoint.rotation;
+        }
+
+        private Portal GetOtherPortal()
+        {
+            var otherPortal = FindObjectsOfType<Portal>().Where(x => x != this).FirstOrDefault();
+            return otherPortal;
         }
     }
 
