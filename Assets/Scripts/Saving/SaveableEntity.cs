1f6bc9e4b552379e42b0df3c5228437953e6187e
Arc1llusion <arc1llusion@gmail.com>
2023-02-26 15:48:25 -0500

    Lesson 98

--- Assets/Scripts/Saving/SaveableEntity.cs
+++ Assets/Scripts/Saving/SaveableEntity.cs
@@ -1,14 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 namespace RPG.Saving
 {
+    [ExecuteAlways]
     public class SaveableEntity : MonoBehaviour
     {
+        [SerializeField]
+        private string uniqueIdentifer = "";
+
         public string GetUniqueIdentifier()
         {
-            return string.Empty;
+            return uniqueIdentifer;
         }
 
         public object CaptureState()
@@ -20,6 +26,21 @@ namespace RPG.Saving
         public void RestoreState(object state)
         {
             Debug.Log("Restoring State for " + GetUniqueIdentifier());
+        }
+
+        public void Update()
+        {
+            if (Application.IsPlaying(gameObject)) return;
+            if (string.IsNullOrEmpty(gameObject.scene.path)) return;
+
+            var serializedObject = new SerializedObject(this);
+            var property = serializedObject.FindProperty(nameof(uniqueIdentifer));            
+
+            if (string.IsNullOrEmpty(property.stringValue))
+            {
+                uniqueIdentifer = System.Guid.NewGuid().ToString();
+                serializedObject.ApplyModifiedProperties();
+            }
         }
     }
 }
