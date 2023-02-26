467162b5177f43b1a384c21907df6baecf9fa210
Arc1llusion <arc1llusion@gmail.com>
2023-02-26 16:00:06 -0500

    Lesson 99

--- Assets/Scripts/Saving/SaveableEntity.cs
+++ Assets/Scripts/Saving/SaveableEntity.cs
@@ -1,8 +1,10 @@
+using RPG.Core;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.AI;
 
 namespace RPG.Saving
 {
@@ -19,28 +21,34 @@ namespace RPG.Saving
 
         public object CaptureState()
         {
-            Debug.Log("Capturing State for " + GetUniqueIdentifier());
-            return null;
+            return new SerializableVector3(transform.position);
         }
 
         public void RestoreState(object state)
         {
-            Debug.Log("Restoring State for " + GetUniqueIdentifier());
+            var sv3 = state as SerializableVector3;
+            var agent = GetComponent<NavMeshAgent>();
+            var actionSchedular = GetComponent<ActionScheduler>();
+
+            agent.Warp(sv3.ToVector());
+            actionSchedular.CancelCurrentAction();
         }
 
         public void Update()
         {
+#if UNITY_EDITOR
             if (Application.IsPlaying(gameObject)) return;
             if (string.IsNullOrEmpty(gameObject.scene.path)) return;
 
             var serializedObject = new SerializedObject(this);
-            var property = serializedObject.FindProperty(nameof(uniqueIdentifer));            
+            var property = serializedObject.FindProperty(nameof(uniqueIdentifer));
 
             if (string.IsNullOrEmpty(property.stringValue))
             {
                 uniqueIdentifer = System.Guid.NewGuid().ToString();
                 serializedObject.ApplyModifiedProperties();
             }
+#endif
         }
     }
 }
